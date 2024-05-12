dd9840ada4e8473e3cb75afcd4a8e6bd08595e13
Eric Bowman <ebowman@boboco.ie>
2024-05-12 15:49:08 +0200

    Now starts processing right away, but thread counts not yet configurable

--- src/main/scala/PDFPasswordChecker.scala
+++ src/main/scala/PDFPasswordChecker.scala
@@ -1,26 +1,30 @@
-import org.apache.pdfbox.pdmodel.PDDocument
 import zio.*
+import zio.console.*
 
 import java.io.File
-import scala.jdk.CollectionConverters.*
+import org.apache.pdfbox.pdmodel.PDDocument
 import org.apache.pdfbox.pdmodel.encryption.InvalidPasswordException
 
 object PDFPasswordChecker extends zio.App {
 
-  def findPDFs(directory: File): Task[List[File]] = {
-    // Obtain all files and directories within the current directory
-    val filesAndDirs = Task(directory.listFiles()).map(_.toList.filter(_ != null))
+  def processFile(file: File): ZIO[Console, Throwable, Unit] = {
+    isPasswordProtected(file).flatMap { isProtected =>
+      if (isProtected) putStrLn(s"${file.getPath} is password protected")
+      else ZIO.unit
+    }
+  }
 
-    // Separate files and directories
-    val pdfFiles = filesAndDirs.flatMap(all => Task(all.filter(f => f.isFile && f.getName.endsWith(".pdf"))))
-    val dirs = filesAndDirs.flatMap(all => Task(all.filter(_.isDirectory)))
+  def findAndProcessPDFs(directory: File): ZIO[Console, Throwable, Unit] = {
+    if (directory.isDirectory) println(s"Processing directory: ${directory.getPath}")
+    val filesAndDirs = Task(Option(directory.listFiles()).getOrElse(Array.empty[File]))
 
-    // Recursively find PDFs in subdirectories
     for {
-      files <- pdfFiles
-      directories <- dirs
-      nestedFiles <- ZIO.foreach(directories)(findPDFs).map(_.flatten)
-    } yield files ++ nestedFiles
+      all <- filesAndDirs
+      pdfFiles = all.filter(f => f.isFile && f.getName.endsWith(".pdf"))
+      dirs = all.filter(_.isDirectory)
+      _ <- ZIO.foreachPar(pdfFiles)(processFile)
+      _ <- ZIO.foreachPar(dirs)(findAndProcessPDFs)
+    } yield ()
   }
 
   def isPasswordProtected(file: File): Task[Boolean] = Task {
@@ -31,15 +35,7 @@ object PDFPasswordChecker extends zio.App {
     case _: InvalidPasswordException => Task.succeed(true)
   }
 
-  def run(args: List[String]): URIO[zio.ZEnv, ExitCode] = {
-    val rootPath = new File("/Users/ebowman/Downloads")
-    findPDFs(rootPath).flatMap { files =>
-      ZIO.foreachPar(files) { file =>
-        isPasswordProtected(file).flatMap { isProtected =>
-          if (isProtected) console.putStrLn(s"${file.getPath} is password protected")
-          else ZIO.unit
-        }
-      }
-    }.exitCode
-  }
+  def run(args: List[String]): URIO[ZEnv, ExitCode] =
+    val rootPath = new File("/Users/ebowman/")
+    findAndProcessPDFs(rootPath).exitCode
 }
